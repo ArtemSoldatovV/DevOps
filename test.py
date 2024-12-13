5ca337a21302fbd20f78e1d9ea8efea05c4face7
ArtemSoldatovV <solvladislav777@gmail.com>
2024-12-13 18:16:22 +0300

    Update test.py

--- test.py
+++ test.py
@@ -8,11 +8,4 @@ assert line_length("int") == 3
 assert line_length("") == "N"
 assert line_length("    ") == "N"
 
-def line_in_the_file(abc):
-  f = open("File.txt", "w+")
-  f.write(abc)
-  f.close()
 
-line_in_the_file("Hi 123456789")
-f = open("File.txt", "r")
-assert f != None
